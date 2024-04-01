881ffc9c84d28af7632500e2dd0c3f6165507b21
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-04-01 21:01:07 +0200

    Create new_main.py

---
+++ new_main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+df=pd.read_csv("df_choro.csv")
+dims=df.columns[2:-6]
+dimDesc={}
+countries=df["country"].unique().tolist()
+dimDict={}
+for el in dims:
+  dimDict[el]={}
+  dimlst=df[el].tolist()
+  for c in countries:
+    cdf=df[df["country"]==c]
+    lst=cdf[el].tolist()
+    dimDict[el][c]=lst
+  mini=min(dimlst)
+  maxi=max(dimlst)
+  if mini>0:
+     dimDict[el]["mini"]=mini
+  else:
+     dimDict[el]["mini"]=0
+  dimDict[el]["maxi"]=maxi
+
+with open('dimsDict.js', 'w') as f:
+    print('export let dimsDict=',dimDict,file=f)  
